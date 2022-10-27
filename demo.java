e06c000b611fc675db5d020e5f001d94bbcc3d8e
mohitpathak23 <90863175+mohitpathak23@users.noreply.github.com>
2022-10-27 00:44:08 -0800

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,1 +1,8 @@
 ![Quote](https://github-readme-quotes.herokuapp.com/quote?theme=dark&animation=grow_out_in)
+         
+         
+ class Main {
+   public static void main(String[] args) {
+     System.out.println("Hello");
+   }
+ }
